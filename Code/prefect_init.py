1df899eb65e7ddf6652a9d3d1c174ccd454e0e49
Anusha Gururaj Jamkhandi <ajamkhandi@squareup.com>
2023-09-13 09:43:46 -0700

    Delete some Notebooks

--- Code/prefect_init.py
+++
@@ -1,9 +0,0 @@
-import prefect 
-
-from prefect import task, Flow 
-@task 
-def hello_task(): 
-logger = prefect.context.get("logger") 
-logger.info("Hello world!") 
-flow = Flow("hello-flow", tasks=[hello_task]) 
-flow.run()
